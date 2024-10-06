11989150e0dae254d39a6ec7f1ffc93939905fc6
CrisBryann <nguyenphuocthienan2018cm@gmail.com>
2024-10-06 18:56:39 +0700

    thienan

--- src/api/custom.ts
+++ src/api/custom.ts
@@ -1,18 +1,27 @@
 "use server";
 
 import { httpBag } from "@/lib/http";
-import { TCreateCustomProductRequest, TCustomResponse } from "@/schema/custom.schema";
+import { TCreateCustomProductRequest, TCustomResponse, TUpdateCustomProductRequest } from "@/schema/custom.schema";
 import { TTableResponse } from "@/types/Table";
 import { revalidateTag } from "next/cache";
 
-// Lấy tất cả tài khoản
 export const getAllCustoms = async (params?: any) => {
   const response = await httpBag.get<TTableResponse<TCustomResponse>>(
     "/product-customization",
     {
       params,
       next: { tags: ["custom"] },
+    }
+  );
+  return response;
+};
 
+export const getAllCustomsByUserId = async (userId: string, params?: any) => {
+  const response = await httpBag.get<TTableResponse<TCustomResponse>>(
+    `/product-customization/user/${userId}`,
+    {
+      params,
+      next: { tags: ["custom"] },
     }
   );
   return response;
@@ -38,4 +47,20 @@ export const createCustomProduct = async (
   revalidateTag("customs");
 
   return response;
+};
+
+
+export const getCustomProductById = async (id: string) => {
+  return await httpBag.get<TCustomResponse>(`/product-customization/${id}`, {
+    next: { tags: ["customs"] },
+  });
+};
+
+export const updateCustomProduct = async (body: TUpdateCustomProductRequest) => {
+  const result = await httpBag.patch<TCustomResponse>(
+    "/product-customization/update",
+    body
+  );
+  revalidateTag("customs");
+  return result;
 };
