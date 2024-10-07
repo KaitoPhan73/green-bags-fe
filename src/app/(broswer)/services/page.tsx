0457efa6dae864b270871606476235d3cfd5699d
khai732003 <khaithinhvui2003@gmail.com>
2024-10-07 23:55:53 +0700

    build-services

--- src/app/(broswer)/services/page.tsx
+++ src/app/(broswer)/services/page.tsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
 
 import React from "react";
 import PageTitle from "@/components/page-tittle";
 import Services from "@/app/_components/services";
 const page = () => {
   return (
-    <div>
+    <div className="container mx-auto px-0 md:px-6 lg:px-8 mb-12">
       <PageTitle title="Dịch Vụ" />
       <div className="container mx-auto px-0 md:px-6 lg:px-8 mb-12"></div>
       <Services />
-    </div >
+    </div>
   );
 };
 
