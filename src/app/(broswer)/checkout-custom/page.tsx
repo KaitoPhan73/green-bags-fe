19535665dbac59dad7894fa6c5d29b36f0352f0b
khai732003 <khaithinhvui2003@gmail.com>
2024-10-09 23:16:56 +0700

    push now

--- src/app/(broswer)/checkout-custom/page.tsx
+++ src/app/(broswer)/checkout-custom/page.tsx
@@ -6,7 +6,7 @@ const CheckoutCustomForm = dynamic(() => import("./_components/checkout"), {
 const page = () => {
   return (
     <div>
-      <PageTitle title="Thiết kế " />
+      <PageTitle title="Thanh toán" />
       <section className="py-12 md:py-24 lg:py-24 max-w-6xl mx-auto">
         {/* <CheckoutCustomForm /> */}
       </section>
